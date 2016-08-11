136f7eb40e7917b2ffd419e410cbab5f687d8c85
Plutonist <widesense.plqws@gmail.com>
2016-08-11 20:27:35 +0800

    Add actionbar for SettingActivity

--- app/src/main/java/ml/puredark/hviewer/activities/SettingActivity.java
+++ app/src/main/java/ml/puredark/hviewer/activities/SettingActivity.java
@@ -1,17 +1,41 @@
 package ml.puredark.hviewer.activities;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ml.puredark.hviewer.R;
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.btn_return)
+    ImageView btnReturn;
+
+    private DrawerArrowDrawable btnReturnIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingFragment()).commit();
+
+        setContentView(R.layout.activity_setting);
+        getFragmentManager().beginTransaction().replace(R.id.setting_content, new SettingFragment()).commit();
+        ButterKnife.bind(this);
+
+        toolbar.setTitle("设置");
+        setSupportActionBar(toolbar);
+
+        /* 为返回按钮加载图标 */
+        btnReturnIcon = new DrawerArrowDrawable(this);
+        btnReturnIcon.setColor(getResources().getColor(R.color.white));
+        btnReturn.setImageDrawable(btnReturnIcon);
+        btnReturnIcon.setProgress(1f);
     }
 
     public static class SettingFragment extends PreferenceFragment
